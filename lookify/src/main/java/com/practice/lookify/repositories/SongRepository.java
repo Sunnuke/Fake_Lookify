49b9d9a6692f524c700e9c529924a98f74765fbe
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-12 18:02:21 -0800

    Created SongService to work with SongRepository interface and to allow controlled functional access to database methods

--- lookify/src/main/java/com/practice/lookify/repositories/SongRepository.java
+++ lookify/src/main/java/com/practice/lookify/repositories/SongRepository.java
@@ -3,9 +3,11 @@ package com.practice.lookify.repositories;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.practice.lookify.models.Song;
 
+@Repository
 public interface SongRepository extends CrudRepository<Song, Long> {
 	
 	List<Song> findAll();
---
+++ lookify/src/main/java/com/practice/lookify/services/SongService.java
@@ -0,0 +1,54 @@
+package com.practice.lookify.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.practice.lookify.models.Song;
+import com.practice.lookify.repositories.SongRepository;
+
+public class SongService {
+	
+	@Autowired
+	private SongRepository songRepository;
+	
+//	RETRIEVE
+	public List<Song> allSongs(){
+		return songRepository.findAll();
+	}
+	
+	
+	public Song findSong(Long id) {
+		Optional<Song> optionalSong = songRepository.findById(id);
+		if (optionalSong.isPresent()) {
+			return optionalSong.get();
+		}
+		else {
+			return null;
+		}
+
+	}
+	
+//	CREATE
+	public Song createLanguage(Song song) {
+		return songRepository.save(song);
+		
+	}
+	
+//	UPDATE	
+	public Song updateLanguage(Song song, Long id) {
+		Song songOg = this.findSong(id);
+		songOg.setTitle(song.getTitle());
+		songOg.setArtist(song.getArtist());
+		songOg.setRating(song.getRating());
+		songRepository.save(this.findSong(id));
+		return this.findSong(id);
+	}
+	
+//	DELETE
+	public void deleteSong(Long id) {
+		songRepository.deleteById(id);;
+	}
+	
+}
